9e5a4d8847f349508360511c3944391de9c5637d
Breakthrough <temitopefatoki658@gmail.com>
2023-09-19 15:44:45 -0700

    Starting afresh

--- printf_project/main.h
+++ printf_project/main.h
@@ -3,10 +3,7 @@
 #include <stdarg.h>
 
 int _putchar(char c);
-void to_print_char(int ch_arg);
-void to_print_string(char *s_arg);
-void to_print_percent(void);
-int conversion_specifiers(const char *format, va_list args);
+
 int _printf(const char *format, ...);
 
 #endif
