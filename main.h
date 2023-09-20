4157024b8564d4aaab9daa1ca08fe4133581d9b0
Breakthrough <temitopefatoki658@gmail.com>
2023-09-20 06:52:18 -0700

    Moved files

--- main.h
+++ main.h
@@ -1,12 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+#include <string.h>
 
-int _putchar(char c);
-void to_print_char(int ch_arg);
-void to_print_string(char *s_arg);
-void to_print_percent(void);
-int conversion_specifiers(const char *format, va_list args);
 int _printf(const char *format, ...);
 
 #endif
