3eb67a12fcd0608ea8a6b8132a87d8e26c93a0bb
Breakthrough <temitopefatoki658@gmail.com>
2023-09-19 18:20:39 -0700

    It works!

--- printf_project/printf0.c
+++ printf_project/printf0.c
@@ -4,47 +4,54 @@
 
 /**
  * _printf - Prints the function
- *@format: prints format
- *@...; elipses
- *Return: number of chracters printed
+ * @format: prints format
+ * *@...: elipses
+ * *Return: number of chracters printed
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
-    int count = 0;
-    while (*format) {
-	    if (*format == '%') {
-		    format++;
-		    if (*format == 'c') 
-			    int char_arg = va_arg(args, int);
-		    putchar(char_arg);
-		    count++;
-	    } else if (*format == 's') {
-		    char *str_arg = va_arg(args, char *);
-		    while (*str_arg) {
-			    putchar(*str_arg);
-			    str_arg++;
-			    count++;
-		    }
-	    } else if (*format == '%') {
-                putchar('%');
-                count++;
-	    }
-    } else {
-	    putchar(*format);
-	    count++;
- }
-    format++;
+	int count, char_arg;
+	char *str_arg;
+	va_list args;
+
+	va_start(args, format);
+	count = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char_arg = va_arg(args, int);
+
+				putchar(char_arg);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				str_arg = va_arg(args, char *);
+
+				while (*str_arg)
+				{
+					putchar(*str_arg);
+					str_arg++;
+					count++;
+				}
+			} else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
-    va_end(args);
-    return count;
-}
-
-
-
-
-
-
-
-
