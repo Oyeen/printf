c34c89683569e04fd2d15d146a44dc7700d2e4f2
Oyeen <oyindamolaayoola5@gmail.com>
2023-09-19 18:53:21 -0700

    error

--- printf_project/printf0.c
+++ printf_project/printf0.c
@@ -1,56 +1,54 @@
-#include <stdarg.h>
 #include <stdio.h>
-#include "main.h"
+#include <stdareg.h>
 
-/**
- * _printf - Prints the function
- * @format: prints format
- * *@...: elipses
- * *Return: number of chracters printed
- */
 int _printf(const char *format, ...)
 {
-	int count, char_arg;
-	char *str_arg;
 	va_list args;
 
 	va_start(args, format);
-	count = 0;
+	int count = 0;
+	char ch;
+	const char *str;
 
-	while (*format)
+	while (*format != '\0')
+
 	{
-		if (*format == '%')
+		if (format == '%')
 		{
 			format++;
-			if (*format == 'c')
+			switch (*format)
 			{
-				char_arg = va_arg(args, int);
-
-				putchar(char_arg);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				str_arg = va_arg(args, char *);
-
-				while (*str_arg)
-				{
-					putchar(*str_arg);
-					str_arg++;
+				case 'c':
+					ch = (char) va_arg(args, int);
+					putchar(ch);
 					count++;
-				}
-			} else if (*format == '%')
-			{
-				putchar('%');
-				count++;
+					break;
+				case 's':
+					str = va_arg(args, const char *);
+					while (*str != '\0')
+					{
+						putchar(*str);
+						str++;
+						count++;
+					}
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					putchar('%');
+					putchar(*format);
+					count += 2;
+					break;
 			}
 		}
-		else
-		{
-			putchar(*format);
-			count++;
-		}
-		format++;
+	 else
+	 {
+		 putchar(*format);
+		 count++;
+	 }
+	 	format++;
 	}
 	va_end(args);
 	return (count);
